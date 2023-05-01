f29c3ac6d9a51e23b413a27fe5ea70bfc39c3519
Vladbolo <bolo19514@gmail.com>
2023-05-01 15:20:22 +0300

    feat: create keyboard only english launguage without styles

--- index.js
+++ index.js
@@ -1,13 +1,106 @@
-const engSmall = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
-  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
-  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift',
-  'Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '←', '↓', '→'];
+const keysEnglish = [
+  '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
+  'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'DEL',
+  'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\',', 'Enter',
+  'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift',
+  'Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', '◄', '▼', '►'
+]
 
-const engShift = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Backspace", 
-"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|",
-"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "Enter",
-"Shift", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "↑", "Shift",
-"Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "←", "↓", "→"];
+const inputField = document.createElement("textarea");
+const keyboard = document.createElement("div");
 
-const rusSmall = ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", "."];
+
+inputField.className = 'textarea'
+keyboard.setAttribute("id", "keyboard");
+
+document.body.appendChild(inputField);
+document.body.appendChild(keyboard);
+
+keysEnglish.forEach(key => {
+  const button = document.createElement("button");
+  button.setAttribute("class", "key");
+  button.innerHTML = key;
+  keyboard.appendChild(button);
+})
+
+const keyButtons = document.querySelectorAll(".key");
+let isCapsOn = false;
+
+const setLowerCaseKeys = () => {
+  for (const key of keyButtons) {
+    if (key.innerHTML.length === 1) {
+      key.innerHTML = key.innerHTML.toLowerCase()
+    }
+  }
+}
+const setUpperCaseKeys = () => {
+  for (const key of keyButtons) {
+    if (key.innerHTML.length === 1) {
+      key.innerHTML = key.innerHTML.toUpperCase()
+    }
+  }
+}
+
+//  Spacebar
+keyButtons[58].addEventListener('click', () => {
+  inputField.innerHTML += ' '
+})
+
+// Tab
+keyButtons[14].addEventListener('click', () => {
+  inputField.innerHTML += '   '
+})
+
+// Enter
+keyButtons[41].addEventListener('click', () => {
+  inputField.innerHTML += '\n'
+})
+
+// L-Shift
+keyButtons[42].addEventListener('mousedown', () => {
+  setUpperCaseKeys()
+})
+keyButtons[42].addEventListener('mouseup', () => {
+  setLowerCaseKeys()
+})
+
+// R-Shift
+keyButtons[54].addEventListener('mousedown', () => {
+  setUpperCaseKeys()
+})
+keyButtons[54].addEventListener('mouseup', () => {
+  setLowerCaseKeys()
+})
+
+//  Capslock
+keyButtons[28].addEventListener('click', () => {
+  isCapsOn = !isCapsOn;
+  if (isCapsOn === true) {
+    keyButtons[28].style.backgroundColor = 'green'
+    setUpperCaseKeys()
+  } else {
+    keyButtons[28].style.backgroundColor = '#1C232E'
+    setLowerCaseKeys()
+  }
+})
+
+// Backspace
+keyButtons[13].addEventListener('click', () => {
+  if (inputField.innerHTML !== undefined) {
+    inputField.innerHTML = inputField.innerHTML.slice(0, inputField.innerHTML.length - 1)
+  }
+})
+
+//Del
+keyButtons[27].addEventListener('click', () => {
+  inputField.innerHTML = '';
+})
+
+
+for (const key of keyButtons) {
+  if (key.innerHTML.length === 1) {
+    if(key.innerHTML !== '▲' && key.innerHTML !== '►' && key.innerHTML !==  '▼' && key.innerHTML !== '◄'){
+      key.addEventListener('click', () => inputField.innerHTML += key.innerHTML)
+      }
+  }
+}
